6ce5c6b9c3ea85a4e010618da432bc4546f47213
desantis06 <schueler.bdesantis@tbk-solingen.de>
2025-11-26 08:00:50 +0100

    start

---
+++ mssql.php
@@ -0,0 +1,8 @@
+<?php 
+$serverName = "(localdb)\MSSQLLocalDB";
+$database = "Projekt";
+$username = "admin";
+$password = "admin";
+ 
+$pdo = new PDO("sqlsrv:Server=$serverName;Database=$database", $username, $password);
+?>
