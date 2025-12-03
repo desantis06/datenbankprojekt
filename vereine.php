ad5300b8ebe1623daad36baf03df8da57c144332
DB-Projekt-Boss <schueler.jkremer@tbk-solingen.de>
2025-12-03 08:14:21 +0100

    vereine-add zum kopieren

--- vereine.php
+++ vereine.php
@@ -23,13 +23,24 @@ $pdo = null;
         <th>PK_Verein</th>
         <th>Name</th>
         <th>Gründungsjahr</th>
+        <th>Ändern</th>
+        <th>Löschen</th>
     </tr>
 
     <?php foreach ($vereine as $row): ?>    
     <tr>
-        <td><?= htmlspecialchars($row['PK_Verein']) ?></td>
-        <td><?= htmlspecialchars($row['Name']) ?></td>
-        <td><?= htmlspecialchars($row['Gruendungsjahr']) ?></td>
+        <td><?= $row['PK_Verein'] ?></td>
+        <td><input type="text" value="<?= htmlspecialchars($row['Name']) ?>"></td>
+        <td><input type="text" value="<?= htmlspecialchars($row['Gruendungsjahr']) ?>"></td>
+        <td><button type="submit" name="change" value="<?= $row['PK_Verein'] ?>">Ändern</button></td>
+        <td><button type="submit" name="delete" value="<?= $row['PK_Verein'] ?>">Löschen</button></td>
     </tr>
     <?php endforeach; ?>
+
+    <form action="./vereine/vereine-add.php" method="post">
+        <td>auto increment</td>
+        <td><input type="text" name="vereinName"></td>
+        <td><input type="text" name="gruendungsjahr"></td>
+        <td><button type="submit" name="add">Hinzufügen</button></td>
+    </form>
 </table>
---
+++ vereine/vereine-add.php
@@ -0,0 +1,23 @@
+<?php include '../navigator.php'; ?>
+<?php include '../database/mssql.php'; 
+
+print_r($_POST);?>
+
+<?php
+try {
+    $stmt = $pdo->prepare("
+        INSERT INTO tbl_verein ([Name], [Gruendungsjahr])
+        VALUES (:vereinName, :jahr)
+    ");
+    $stmt->bindParam(':vereinName', $_POST["vereinName"]);
+    $stmt->bindParam(':jahr', $_POST["gruendungsjahr"]);
+
+    $stmt->execute();
+    //$vereine = $stmt->fetchAll();   
+
+} catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+$pdo = null;
+?>
