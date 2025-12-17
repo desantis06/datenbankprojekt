e5192b36d84ca4d6b93610a7a6c843d4cc9c122e
adreking <schueler.dseibel@tbk-solingen.de>
2025-12-17 08:11:51 +0100

    sdfsdf

---
+++ Schiedsrichter/Schiedsrichter_update.php
@@ -0,0 +1,27 @@
+<?php include '../navigator.php'; ?>
+<?php include '../database/mssql.php'; 
+
+print_r($_POST);?>
+
+<?php
+try {
+    $stmt = $pdo->prepare("
+        UPDATE tbl_spieler
+        SET Vorname = :vname, Nachname = :nname ,Lizensenschein = :schein, 
+        WHERE PK_Schiedsrichter = :pk
+    ");
+    $stmt->bindParam(':vname', $_POST["vname"]);
+    $stmt->bindParam(':nname', $_POST["nname"]);
+    $stmt->bindParam(':schein', $_POST["schein"]);
+    $stmt->bindParam(':pk', $_POST["PK_Schiedsrichter"]);
+
+    $stmt->execute();  
+
+    header('Location: ../Schiedsrichter.php');
+
+} catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+$pdo = null;
+?>
--- schiedrichter.php
+++ schiedrichter.php
@@ -1,2 +1,54 @@
-<?php include 'navigator.php' ?>
-<?php include 'database/mssql.php'?>
+
+<?php include 'navigator.php'; ?>
+<?php include 'database/mssql.php'; ?>
+
+<?php
+try {
+    $stmt = $pdo->prepare(
+        "SELECT [PK_Schiedsrichter], [Vorname], [Nachname],[Lizenzschein]
+         FROM tbl_Schiedsrichter"
+    );
+
+    $stmt->execute();
+    $schiedsrichter = $stmt->fetchAll();   
+
+} catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+$pdo = null;
+?>
+
+<table cellpadding="7">
+    <tr>
+        <th>PK_Schiedsrichter</th>
+        <th>Vorname</th>
+        <th>Nachname</th>
+        <th>Lizenzschein</th>
+    </tr>
+
+    <?php foreach ($schiedsrichter as $row): ?>    
+    <tr>
+        <form action="./Schiedsrichter/Schiedsrichter_update.php" method="post">
+            <td><?= $row['PK_Schiedsrichter'] ?></td>
+            <td><input type="text" name="vname" value="<?= $row['Vorname'] ?>"></td>
+            <td><input type="text" name="nname" value="<?= $row['Nachname'] ?>"></td>
+            <td><input type="text" name="schein" value="<?= $row['Lizenzschein'] ?>"></td>
+            <!-- <input type="hidden" name="PK_Verein" value="<?= $row['PK_Schiedsrichter'] ?>"> -->
+            <td><button type="submit" name="PK_Schiedsrichter" value="<?= $row['PK_Schiedsrichter'] ?>">Ändern</button></td>
+        </form>
+        <form action="./Schiedsrichter/Schiedsrichter_delete.php" method="post">
+            <td><button type="submit" name="PK_Schiedsrichter" value="<?= $row['PK_Schiedsrichter'] ?>">Löschen</button></td>
+        </form>
+        
+    </tr>
+    <?php endforeach; ?>
+
+    <form action="./Schiedsrichter/Schiedsrichter_add.php" method="post">
+        <td>auto increment</td>
+        <td><input type="text" name="vname"></td>
+        <td><input type="text" name="nname"></td>
+        <td><input type="text" name="schein"></td>
+        <td><button type="submit" name="add">Hinzufügen</button></td>
+    </form>
+</table>
